6aac51db7a565a6fbe59e6c46d7857562dd3b3c3
KJ-M <rfb0920@gmail.com>
2024-06-19 16:56:54 +0900

    ファイルを作成して、buttonを押すと数値が増える機能を実装しました。

---
+++ script.js
@@ -0,0 +1,15 @@
+
+// 1 increase buttonが押されるとcounter-numberが増える
+
+
+const $button = document.getElementById("js-button");
+const $counter = document.getElementById("js-counter");
+
+$button.addEventListener("click", () => {
+    let currentCount = parseInt($counter.textContent); //counterの文字列を数値に変える
+    $counter.textContent = currentCount + 1; //counterの数値が１つずつ大きくなっていく
+});
+
+
+
+
