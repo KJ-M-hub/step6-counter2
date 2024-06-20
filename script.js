fa770aecd081b827749f6bcef08b19efe94ee153
KJ-M <rfb0920@gmail.com>
2024-06-20 13:35:22 +0900

    コードを追記したり修正したりしました。 一旦コミットします。

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
+(() => {
 
-// 1 increase buttonが押されるとcounter-numberが増える
+
+    const $button = document.getElementById("js-button");
+    const $counter = document.getElementById("js-counter");
+   
+    $button.addEventListener("click", () => {
+        let currentCount = parseInt($counter.textContent); //counterの文字列を数値に変える
+        $counter.textContent = currentCount + 1; //counterの数値が１つずつ大きくなっていく
+    });
 
 
-const $button = document.getElementById("js-button");
-const $counter = document.getElementById("js-counter");
 
-$button.addEventListener("click", () => {
-    let currentCount = parseInt($counter.textContent); //counterの文字列を数値に変える
-    $counter.textContent = currentCount + 1; //counterの数値が１つずつ大きくなっていく
-});
+
+
+
+})();
+
+
+
 
 
 
